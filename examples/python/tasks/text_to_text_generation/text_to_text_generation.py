078a0572a05ac2b64a2c3995c5acfd0b369ab64c
Aaron Vogler <aaronvogler@gmail.com>
2024-11-06 16:03:46 -0500

    Refine python examples.

--- examples/python/tasks/text_to_text_generation/text_to_text_generation.py
+++ examples/python/tasks/text_to_text_generation/text_to_text_generation.py
@@ -2,14 +2,13 @@ from bytez import Bytez
 
 client = Bytez("YOUR BYTEZ KEY HERE")
 
-model_params = {"max_new_tokens": 20, "temperature": 2}
-
 model = client.model("google/flan-t5-base")
 
 model.load()
 
-
 input_text = "Once upon a time there was a small little man who"
+
+model_params = {"max_new_tokens": 20, "temperature": 2}
 
 result = model.run(input_text, model_params=model_params)
 
