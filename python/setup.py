995f883b3fc70de5a305034105834d69a409ff59
Aaron Vogler <aaronvogler@gmail.com>
2024-11-14 18:23:05 -0500

    Update python version.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytez",  # Name of your package
-    version="0.2.26",
+    version="0.2.28",
     packages=find_packages(),
     description="Python API client for Bytez service",
     long_description=open("./README.md").read(),
