d0708c8eaa3f6a1a84d2e9e84f0f63ba9094a595
NawarA <nawar@bytez.com>
2025-10-27 12:52:54 -0700

    bump (update readmes)

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytez",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     description="Python API client for Bytez service",
     long_description=open("./README.md").read(),
