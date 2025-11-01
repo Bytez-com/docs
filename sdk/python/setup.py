d6644d48adc2c2a32beb481c15934067623f560b
NawarA <nawar@bytez.com>
2025-11-01 10:30:35 -0700

    add key as a prop

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytez",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(),
     description="Python API client for Bytez service",
     long_description=open("./README.md").read(),
